c7a990f66ba0a3316d303ce4dd4e5fd4280aa56b
Linh Bui <linh@Linhs-iMac.local>
2023-01-26 22:09:29 -0800

    generating README.file

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 const question = [
     {   
         type: 'input',
@@ -28,15 +29,31 @@ inquirer
             type: 'input',
             message: 'how to install it?',
             name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'what did you learn',
+            name: 'knowledge'
         }
     ])
     .then((answer)=>{
-        console.log(answer)
-        console.log(answer.title)
-        console.log(answer.purpose)
-        console.log(answer.instruction)
-        console.log(answer.installation)
+        const template = `# ${answer.title}
+        * [Installation][#Installation]
+        # Installation
+        ${answer.installation}
+        ## purpose
+        ${answer.purpose} 
+        `
+        newFile(answer.title,template)
     })
-    .catch((error)=>{
-
+    .catch((error)=>{ 
+        console.log(error)
     })
+    function newFile(title,template){ // 
+        fs.writeFile(`./${title.toLowerCase().split(' ').join('')}.md`,template,(err)=>{
+            if(err){
+                console.log(err)
+            }
+            console.log('Your README file has been created')
+        })
+    }
